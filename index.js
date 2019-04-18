20040ffda13c400df3f4beeee7204ff6fc002b72
Nekwan Luke <contact@nekwanluke.com>
2019-04-18 16:11:26 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function takeANumber (katzDeli, name) {
+  var katzDeliLine = [];
+  katzDeli.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+function nowServing(katzDeli) {
+  let i= 0
+  while (i < katzDeli.length) {
+    i++;
+  }
+  if (katzDeli.length === 0){
+    return "There is nobody waiting to served!";
+  }
+  else
+  return (`Currently serving ${katzDeli.shift()}.`);
+}
+
+
