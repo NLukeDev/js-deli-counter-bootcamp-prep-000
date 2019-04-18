97e17c72ea37405e288ee33abcb7b288678ab842
Nekwan Luke <contact@nekwanluke.com>
2019-04-18 16:12:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function nowServing(katzDeli) {
     i++;
   }
   if (katzDeli.length === 0){
-    return "There is nobody waiting to served!";
+    return "There is nobody waiting to be served!";
   }
   else
   return (`Currently serving ${katzDeli.shift()}.`);
